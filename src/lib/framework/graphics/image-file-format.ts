9eeda463e9e28bd795c462084cb69cef31cf0d30
cat1989 <562702740@qq.com>
2021-12-14 09:30:48 +0800

    feat: ImageFileFormat

---
+++ src/lib/framework/graphics/image-file-format.ts
@@ -0,0 +1,14 @@
+/**
+ * Defines supported image file formats that may be used for textures.
+ */
+export enum ImageFileFormat {
+    /**
+     * Joint Photographic Experts Group (JPEG) compressed file format.
+     */
+    Jpg,
+
+    /**
+     * Portable Network Graphics file format.
+     */
+    Png,
+}
