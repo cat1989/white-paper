ad3afabe7e762479714122fbed845bff59954290
cat1989 <562702740@qq.com>
2021-12-14 09:31:51 +0800

    feat: add some new features, such as load a texture2d from a file, save a texture2d

--- src/lib/framework/graphics/texture-2d.ts
+++ src/lib/framework/graphics/texture-2d.ts
@@ -1,6 +1,7 @@
 import { Texture } from './texture'
 import { GraphicsDevice } from './graphics-device'
 import { ResourceType } from './resource-type'
+import { ImageFileFormat } from './image-file-format';
 
 /**
  * Represents a 2D grid of texels.
@@ -46,5 +47,57 @@ export class Texture2D extends Texture {
      */
     get width() {
         return this.image.width
+    }
+
+    /**
+     * Creates a texture resource from a file.
+     * @param graphicsDevice The graphics device to use to create the texture resource.
+     * @param filename The name of the file containing the texture.
+     */
+    static fromFile(
+        graphicsDevice: GraphicsDevice,
+        filename: string,
+    ) {
+        return new Promise<Texture2D>((resolve, reject) => {
+            const texture2D = new Texture2D(graphicsDevice)
+            texture2D.name = filename
+            texture2D.image.onload = () => {
+                resolve(texture2D)
+            }
+            texture2D.image.onerror = () => {
+                reject()
+            }
+        })
+    }
+
+    /**
+     * Saves a texture to a file.
+     * @param filename The file name of the destination image.
+     * @param format The file format to use when saving.
+     */
+    save(
+        filename: string,
+        format: ImageFileFormat,
+    ) {
+        const canvas = document.createElement("canvas")
+        canvas.width = this.width
+        canvas.height = this.height
+        const context = canvas.getContext("2d")
+        if (context) {
+            context.drawImage(this.image, 0, 0)
+            let type = ''
+            switch (format) {
+                case ImageFileFormat.Jpg:
+                    type = 'jpg'
+                    break
+                case ImageFileFormat.Png:
+                    type = 'png'
+                    break
+            }
+            const a = document.createElement("a")
+            a.download = filename
+            a.href = canvas.toDataURL(type)
+            a.click()
+        }
     }
 }
--- src/lib/framework/graphics/texture.ts
+++ src/lib/framework/graphics/texture.ts
@@ -1,7 +1,18 @@
 import { GraphicsResource } from './graphics-resource'
+import { GraphicsDevice } from './graphics-device'
 /**
  * Represents a texture resource.
  */
 export abstract class Texture extends GraphicsResource {
-
+    /**
+     * Creates a texture resource from a file.
+     * @param graphicsDevice The graphics device to use to create the texture resource.
+     * @param filename The name of the file containing the texture.
+     */
+    static fromFile(
+        graphicsDevice: GraphicsDevice,
+        filename: string,
+    ) {
+        
+    }
 }
